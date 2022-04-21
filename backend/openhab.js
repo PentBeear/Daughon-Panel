98b80dc8f96f93cdb204e7c021f6913804b0dcc2
PentBeear <43016309+PentBeear@users.noreply.github.com>
2022-04-21 11:41:19 -0500

    Update openhab.js

--- backend/openhab.js
+++ backend/openhab.js
@@ -10,6 +10,8 @@ if (submenuList.includes("{")){
   submenuJson = JSON.parse(submenuList.toString());
 }
 
+// Maybe make this into a commonjs library?
+
 var HSBType = Java.type('org.openhab.core.library.types.HSBType');
 
 // Page List
@@ -24,20 +26,20 @@ var pageJSON = `{
 	}, { 
 		"default":"{-action-:-page-,-data-:{-type-:-0-,-name-:-Bedroom-,-info-:[-btn=0,type=5,name=Fan,icon=,state=\${convertState(items.getItem('BedroomFan_Power').state.toString())}-,-btn=1,type=5,name=Closet,icon=,state=\${convertState(items.getItem('BedroomClosetLight_Power').state.toString())}-,-btn=2,type=1,name=Light,icon=,state=\${convertState(items.getItem('BedroomLightOne_Power').state.toString())}-,-btn=3,type=7,name=Scenes,icon=,state=0-,-btn=4,type=4,name=hidden,icon=2-,-btn=5,type=2,name=hidden,icon=0,state=0-]}}", 
 		"btns": [{ 
-			"depths": ["page0btn0depth-1", "page0btn0depth0", "{-action-:-page-,-data-:{-type-:-0-,-name-:-gawd-,-info-:[-btn=0,type=7,name=Blue,icon=,state=0-,-btn=1,type=6,name=Off,icon=,state=-,-btn=7,type=1,name=e,icon=,state=0-,-btn=3,type=7,name=hidden,icon=,state=0-,-btn=4,type=4,name=hidden,icon=2-,-btn=5,type=2,name=hidden,icon=0,state=0-]}}"] 
+			"depths": ["page1btn0depth0", "page1btn0depth1", "page1btn0depth2"] 
 		}, { 
-			"depths": ["page0btn1depth-1", "page0btn1depth0", "page0btn1depth1"] 
+			"depths": ["page1btn1depth0", "page1btn1depth1", "page1btn1depth2"] 
 		}, { 
-			"depths": ["e", "{-action-:-page-,-data-:{-type-:-0-,-name-:-cheese-,-info-:[-btn=0,type=7,name=nest,icon=,state=0-,-btn=1,type=6,name=Off,icon=,state=-,-btn=7,type=1,name=e,icon=,state=0-,-btn=3,type=7,name=hidden,icon=,state=0-,-btn=4,type=4,name=hidden,icon=2-,-btn=5,type=2,name=hidden,icon=0,state=0-]}}", "e"] 
+			"depths": ["page1btn2depth0", "page1btn2depth1", "page1btn2depth2"] 
 		}, { 
-			"depths": ["{-action-:-page-,-data-:{-type-:-0-,-name-:-Bedroom-,-info-:[-btn=0,type=6,name=Blue,icon=,state=0-,-btn=1,type=6,name=Off,icon=,state=-,-btn=2,type=7,name=e,icon=,state=0-,-btn=3,type=7,name=hidden,icon=,state=0-,-btn=4,type=4,name=hidden,icon=2-,-btn=5,type=2,name=hidden,icon=0,state=0-]}}", "e", "e"] 
+			"depths": ["{-action-:-page-,-data-:{-type-:-0-,-name-:-Bedroom-,-info-:[-btn=0,type=6,name=Blue,icon=,state=0-,-btn=1,type=6,name=Off,icon=,state=-,-btn=2,type=7,name=hidden,icon=,state=0-,-btn=3,type=7,name=hidden,icon=,state=0-,-btn=4,type=4,name=hidden,icon=2-,-btn=5,type=2,name=hidden,icon=0,state=0-]}}", "e", "e"] 
 		}] 
 	}, { 
 		"default":"", 
 		"btns": [{ 
-			"depths": ["page0btn0depth-1", "page0btn0depth0", "page0btn0depth1"] 
+			"depths": ["page2btn0depth-1", "page2btn0depth0", "page2btn0depth1"] 
 		}, { 
-			"depths": ["page0btn1depth-1", "page0btn1depth0", "page0btn1depth1"] 
+			"depths": ["page2btn1depth-1", "page2btn1depth0", "page2btn1depth1"] 
 		}] 
 	}, { 
 		"default":"", 
@@ -52,6 +54,8 @@ var pageJSON = `{
 
 var pageJSONParsed = JSON.parse(pageJSON);
 
+// General Helper Section
+
 // Converts on and off to 0 and 1
 function convertState(state){
   if (state.toLowerCase()=="on"){
@@ -60,6 +64,10 @@ function convertState(state){
     return "0";
   }
 }
+
+// End General Helper Section
+
+// Page Function Section
 
 // Resets the nesting helper
 function nestReset(){
@@ -94,20 +102,19 @@ function sendPage(pageNum,btn,depth,force){
   if(force==true)
   {
     if(depth<0){ // Make this do the force_disp instead of page action
-      actions.get("mqtt", "mqtt:broker:x").publishMQTT("cmnd/tasmota_bedroom_nspanel/nextion", eval("`" + pageJSONParsed["pages"][pageNum]["default"].replaceAll("-","\"") + "`") ,false); 
+      actions.get("mqtt", "mqtt:broker:x").publishMQTT("cmnd/tasmota_bedroom_nspanel/nextion", eval("`" + pageJSONParsed["pages"][pageNum]["default"].replaceAll("-","\"") + "`").replace('"action":"page"','"action":"force_disp"') ,false); 
     } else {
       // Selects page, btn number, and depth
       actions.get("mqtt", "mqtt:broker:x").publishMQTT("cmnd/tasmota_bedroom_nspanel/nextion", eval("`" + pageJSONParsed["pages"][pageNum]["btns"][btn]["depths"][depth].replaceAll("-","\"") + "`") ,false);  
     }  
   } else {
-    if(depth<0){
-      actions.get("mqtt", "mqtt:broker:x").publishMQTT("cmnd/tasmota_bedroom_nspanel/nextion", eval("`" + pageJSONParsed["pages"][pageNum]["default"].replaceAll("-","\"") + "`") ,false); 
+    if(depth<0){ // If depth is less than 0 we know we are wanting the default page else we know to grab a specific nest
+      actions.get("mqtt", "mqtt:broker:x").publishMQTT("cmnd/tasmota_bedroom_nspanel/nextion", eval("`" + pageJSONParsed["pages"][pageNum]["default"].replaceAll("-","\"") + "`").replace('"action":"page"','"action":"force_disp"') ,false); 
     } else {
       // Selects page, btn number, and depth
       actions.get("mqtt", "mqtt:broker:x").publishMQTT("cmnd/tasmota_bedroom_nspanel/nextion", eval("`" + pageJSONParsed["pages"][pageNum]["btns"][btn]["depths"][depth].replaceAll("-","\"") + "`") ,false);  
     }
   }
-  
 }
 
 // Reads the page requests and chooses which page to send
@@ -119,12 +126,12 @@ function pageHandler(){
     console.log("Requested regular page")
     nestReset();
     nest=false;
-  } else if(jsonObject["update"] == "btn" && jsonObject["data"]["btn"] == "server" && parseInt(jsonObject["data"]["depth"]) > -1){ // Nesting back request
+  } else if(jsonObject["update"] == "btn" && jsonObject["data"]["action"] == "nesting" && jsonObject["data"]["btn"] == "server" && parseInt(jsonObject["data"]["depth"]) > -1){ // Nesting back request
     console.log("Requested nest back")
     nestSubtraction();
     nest=true;
     back=true;
-  } else if (jsonObject["update"] == "btn" && jsonObject["data"]["btn"] != "server" && parseInt(jsonObject["data"]["depth"]) > -1){  // Nesting up request
+  } else if (jsonObject["update"] == "btn" && jsonObject["data"]["action"] == "nesting" && jsonObject["data"]["btn"] != "server" && parseInt(jsonObject["data"]["depth"]) > -1){  // Nesting up request
     console.log("Requested nest up")
     nestAddition();
     nest=true;
@@ -142,20 +149,53 @@ function pageHandler(){
   }
 }
 
+// End Page Function Section
 
 // {"update":"btn","data":{"btn":"0","action":"nesting","page":"1","depth":"0"}}
 // Handles page requests (Note how its else if still so the script doesn't have to search for every possibility)
 // Checks for "request": instead of just request to make sure that the json actually contais the key.
 // {"request":"page","data":{"page":"1"}}
-if (jsonObject["request"] == "page" || jsonObject["update"] == "btn" && jsonObject["data"]["action"] == "nesting" ){ // Sends pages on request and nesting
-  pageHandler();
+
+// Status Handlers
+/*
+This section reads indicators from the panel to set the required information in the variables
+Ex. Home page=0 submenu=-1 depth=-1
+A value of -1 means disabled
+Submenu stores the button used to enter the submenu
+Page stores the current page
+Depth stores the current depth
+All of these allow for full customization of the panel in every screen and also lets you update the panel on a specific screen using the
+refresh command.
+*/
+if(jsonObject["indicator"] == "home"){
+  items.getItem('BedroomNSPanel_Submenu').sendCommand("-1");
+  items.getItem('BedroomNSPanel_Page').sendCommand("0"); // Home Page
+  items.getItem('BedroomNSPanel_Depth').sendCommand("-1"); // -1 when disabled (No Nesting)
+} else if (jsonObject["indicator"] == "sleep"){
+  items.getItem('BedroomNSPanel_Submenu').sendCommand("-1"); // -1 when disabled (No Submenu)
+  items.getItem('BedroomNSPanel_Page').sendCommand("-1"); // -1 when disabled (Sleep)
+  items.getItem('BedroomNSPanel_Depth').sendCommand("-1"); // -1 when disabled (No Nesting)
+} else if (jsonObject["indicator"] == "page_success"){
+  items.getItem('BedroomNSPanel_Submenu').sendCommand("-1"); // -1 when disabled (No Submenu)
+  items.getItem('BedroomNSPanel_Page').sendCommand(jsonObject["data"]["page"]); // Always Page
+  items.getItem('BedroomNSPanel_Depth').sendCommand(jsonObject["data"]["depth"]); // Always Depth
+} else if (jsonObject["indicator"] == "submenu"){
+  items.getItem('BedroomNSPanel_Submenu').sendCommand(jsonObject["data"]["btn"]); // Submenu is -1 when disabled or btn when enabled
+  items.getItem('BedroomNSPanel_Page').sendCommand(jsonObject["data"]["page"]); // Always Page
+  items.getItem('BedroomNSPanel_Depth').sendCommand(jsonObject["data"]["depth"]); // Always Depth
+} else if (jsonObject["indicator"] == "exit_submenu"){
+  items.getItem('BedroomNSPanel_Submenu').sendCommand("-1"); // -1 when disabled (No Submenu)
+// End State Handlers
+// I/O Handlers
+} else if (jsonObject["request"] == "page" || jsonObject["update"] == "btn" && jsonObject["data"]["action"] == "nesting"){ // Sends pages on request and nesting
+  pageHandler(); // Page handler reads jsonObject and submenuJson on its own. Might convert this to a library that takes an argument
 } else if (jsonObject["update"] == "message"){ // Handles item updates
   
   // Checks for a message update before a page one (Messages can have user input that says page, but pages cannot send the word message)
   // Handles message requests using a telegram bot (Gets the message from the panel and then routes it based on the receiver)
   switch (jsonObject["receiver"]){
     case "x": 
-      actions.get("telegram", "telegram:telegramBot:x").sendTelegram(`Message from ${jsonObject["sender"]}:\n${jsonObject["message"]}`)
+      actions.get("telegram", "telegram:telegramBot:x").sendTelegram(x,`Message from ${jsonObject["sender"]}:\n${jsonObject["message"]}`)
       break;
     case "x":
 

b63e56721b3b5e38988de44648fe7b78be570079
PentBeear <43016309+PentBeear@users.noreply.github.com>
2022-04-23 15:57:50 -0500

    Delete backend/libraries/openhab directory

--- backend/libraries/openhab/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    get actionHandler() { return require('./actionHandler.js') },
-    get pageHandler() { return require('./pageHandler.js') },
-    get indicatorHandler() { return require('./indicatorHandler.js') },
-    get messageHandler() { return require('./messageHandler.js') },
-}
-

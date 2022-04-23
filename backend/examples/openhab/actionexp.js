6ea2e21cdb772cd94900f6a4c8dbba928fc3c32e
PentBeear <43016309+PentBeear@users.noreply.github.com>
2022-04-23 17:59:55 -0500

    Update actionexp.js

--- backend/examples/openhab/actionexp.js
+++ backend/examples/openhab/actionexp.js
@@ -3,6 +3,6 @@ function hello(){
   const daughon = require("./daughon_helper")
   var test = new amazon.pages.page(0,"Bedroom",1,-1,-1,'"btn=0,type=5,name=Fan,icon=,state=\${this.#convertState(items.getItem("BedroomFan_Power").state.toString())}","btn=1,type=5,name=Closet,icon=,state=\${this.#convertState(items.getItem("BedroomClosetLight_Power").state.toString())}","btn=2,type=1,name=Light,icon=,state=\${this.#convertState(items.getItem("BedroomLightOne_Power").state.toString())}","btn=3,type=7,name=Scenes,icon=,state=0","btn=4,type=4,name=hidden,icon=2","btn=5,type=2,name=hidden,icon=0,state=0"');
 
-//actions.get("mqtt", "mqtt:broker:9882e1ef27").publishMQTT("cmnd/tasmota_bedroom_nspanel/nextion", daughon.actions.panelActions.wake() ,false);  
-  actions.get("mqtt", "mqtt:broker:9882e1ef27").publishMQTT("cmnd/tasmota_bedroom_nspanel/nextion", daughon.actions.panelActions.vis(3,1) ,false);  
+//actions.get("mqtt", "mqtt:broker:x").publishMQTT("cmnd/tasmota_bedroom_nspanel/nextion", daughon.actions.panelActions.wake() ,false);  
+  actions.get("mqtt", "mqtt:broker:x").publishMQTT("cmnd/tasmota_bedroom_nspanel/nextion", daughon.actions.panelActions.vis(3,1) ,false);  
 }

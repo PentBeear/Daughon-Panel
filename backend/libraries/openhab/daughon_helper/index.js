471f2288ad087143c308bfff78a126f017130fac
PentBeear <43016309+PentBeear@users.noreply.github.com>
2022-04-24 07:42:20 -0500

    Delete backend/libraries/openhab/daughon_helper directory

--- backend/libraries/openhab/daughon_helper/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    get buttons() { return require('./buttonHandler.js') },
-    get pages() { return require('./pageHandler.js') },
-    get indicators() { return require('./indicatorHandler.js') },
-    get messages() { return require('./messageHandler.js') },
-    get actions() { return require('./actions.js') },
-}

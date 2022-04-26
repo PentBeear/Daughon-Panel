d05e296990c0e4e2cae546a355aaec41633a7388
PentBeear <43016309+PentBeear@users.noreply.github.com>
2022-04-26 15:44:31 -0500

    Updated examples for the daughon openhab library to reflect recent changes.

--- backend/examples/openhab/panelexample.js
+++ backend/examples/openhab/panelexample.js
@@ -4,58 +4,54 @@ wrapper(); // To let the garbage collector actually collect...
 function wrapper(){
   const daughon = require("./daughon_helper") // Helper library
   // Gets the request
-  var jsonObject = JSON.parse(this.event.toString().substr(this.event.toString().indexOf("{"),this.event.toString().length()));
+  var update = JSON.parse(this.event.toString().substr(this.event.toString().indexOf("{"),this.event.toString().length()));
   
   // User MODIFIABLE!
     
   // Create a new message handler with your openhab telegram thing
-  var messageHandler = new daughon.messages.messageHandler("telegram:telegramBot:x")
+  //var messageHandler = new daughon.messages.messageHandler("telegram:telegramBot:5e8e4ebfc6")
   
   // Make a list of your people that can be messaged here with their telegram chat id.
-  var people = [new daughon.messages.Person("x",x)];
+  var people = [new daughon.messages.Person("tommy",5203767272)];
   
   // submenuItem, depthItem, pageItem, submenuArrayItem, mqttBroker, commandTopic
   // Create a page handler with your required items and mqtt broker
-  var pageHandler = new daughon.pages.pageHandler("BedroomNSPanel_Submenu","BedroomNSPanel_Depth","BedroomNSPanel_Page","BedroomNSPanel_SubmenuList","mqtt:broker:x","cmnd/tasmota_bedroom_nspanel/nextion")
+  //var pageHandler = new daughon.pages.pageHandler("BedroomNSPanel_Submenu","BedroomNSPanel_Depth","BedroomNSPanel_Page","BedroomNSPanel_SubmenuList","mqtt:broker:9882e1ef27","cmnd/tasmota_bedroom_nspanel/nextion")
   
   // Page List Modify to display what page you want, Don't change the home defaults (page 0)
   // type, name, page, btn, depth, info
   // Create your pages with your types names and etc
-  var pages = [new daughon.pages.page(0,"Bedroom",-1,1,-1,'"btn=0,type=5,name=Fan,icon=,state=\${this.#convertState(items.getItem("BedroomFan_Power").state.toString())}","btn=1,type=5,name=Closet,icon=,state=\${this.#convertState(items.getItem("BedroomClosetLight_Power").state.toString())}","btn=2,type=1,name=Light,icon=,state=\${this.#convertState(items.getItem("BedroomLightOne_Power").state.toString())}","btn=3,type=7,name=Scenes,icon=,state=0","btn=4,type=4,name=hidden,icon=2","btn=5,type=2,name=hidden,icon=0,state=0"'),
-              new daughon.pages.page(0,"Bedroom",3,1,0,'"btn=0,type=6,name=Blue,icon=,state=0","btn=1,type=6,name=Off,icon=,state=0","btn=2,type=7,name=hidden,icon=,state=0","btn=3,type=7,name=hidden,icon=,state=0","btn=4,type=4,name=hidden,icon=2","btn=5,type=2,name=hidden,icon=0,state=0"'),
+  var pages = [new daughon.pages.Page(0,"Bedroom",-1,1,-1,'"btn=0,type=5,name=Fan,icon=,state=\${this.#convertState(items.getItem("BedroomFan_Power").state.toString())}","btn=1,type=5,name=Closet,icon=,state=\${this.#convertState(items.getItem("BedroomClosetLight_Power").state.toString())}","btn=2,type=1,name=Light,icon=,state=\${this.#convertState(items.getItem("BedroomLightOne_Power").state.toString())}","btn=3,type=7,name=Scenes,icon=,state=0","btn=4,type=4,name=hidden,icon=2","btn=5,type=2,name=hidden,icon=0,state=0"'),
+              new daughon.pages.Page(0,"Bedroom",3,1,0,'"btn=0,type=6,name=Blue,icon=,state=0","btn=1,type=6,name=Off,icon=,state=0","btn=2,type=7,name=hidden,icon=,state=0","btn=3,type=7,name=hidden,icon=,state=0","btn=4,type=4,name=hidden,icon=2","btn=5,type=2,name=hidden,icon=0,state=0"'),
               ]; // btn -1 page 1 depth -1, btn 3 page 1 depth 0
   
   // submenuItem, depthItem, pageItem
   // Create your indicator parser with your required items
-  var indicatorHandler = new daughon.indicators.indicatorHandler("BedroomNSPanel_Submenu","BedroomNSPanel_Depth","BedroomNSPanel_Page");
+  //var indicatorHandler = new daughon.indicators.indicatorHandler("BedroomNSPanel_Submenu","BedroomNSPanel_Depth","BedroomNSPanel_Page");
   
   // Submenu handler sends command to refresh submenu on entry
-  var submenuHandler = new daughon.submenus.submenuHandler("mqtt:broker:x","cmnd/tasmota_bedroom_nspanel/nextion");
+  //var submenuHandler = new daughon.submenus.submenuHandler("mqtt:broker:9882e1ef27","cmnd/tasmota_bedroom_nspanel/nextion");
   
   // '{"action":"refresh","data":{"r":"1","g":"1","b":"1","brightness":"\${items.getItem("BedroomLightOne_Color").state.toString().substring(items.getItem("BedroomLightOne_Color").state.toString().lastIndexOf(",") +1)}","w":"\${(items.getItem("BedroomLightOne_White").state.toString())}","t0":"D","t1":"W"}}'
-  var submenus = [new daughon.submenus.submenu(2,1,-1,'{"action":"refresh","data":{"r":"\${Math.round(new HSBType(items.getItem("BedroomLightOne_Color").state.toString()).getRed()*2.55).toString()}","g":"\${Math.round(new HSBType(items.getItem("BedroomLightOne_Color").state.toString()).getGreen()*2.55).toString()}","b":"\${Math.round(new HSBType(items.getItem("BedroomLightOne_Color").state.toString()).getBlue()*2.55).toString()}","t0val":"\${items.getItem("BedroomLightOne_Color").state.toString().substring(items.getItem("BedroomLightOne_Color").state.toString().lastIndexOf(",") +1)}","t1val":"\${(items.getItem("BedroomLightOne_White").state.toString())}","t0":"B","t1":"W"}}')]
+  var submenus = [new daughon.submenus.Submenu(2,1,-1,'{"action":"refresh","data":{"r":"\${Math.round(new HSBType(items.getItem("BedroomLightOne_Color").state.toString()).getRed()*2.55).toString()}","g":"\${Math.round(new HSBType(items.getItem("BedroomLightOne_Color").state.toString()).getGreen()*2.55).toString()}","b":"\${Math.round(new HSBType(items.getItem("BedroomLightOne_Color").state.toString()).getBlue()*2.55).toString()}","t0val":"\${Math.round(parseInt(items.getItem("BedroomLightOne_Color").state.toString().substring(items.getItem("BedroomLightOne_Color").state.toString().lastIndexOf(",") +1))*2.55).toString() }","t1val":"\${(items.getItem("BedroomLightOne_White").state.toString())}","t0":"B","t1":"W"}}')]
 
-  
-  // 
-  
-  
   
   // Creates an actionHandler for every button on every page
   // btn, page, depth
   // Create your actions for every button press (Page and depth specific)
   
-  var buttonHandler = new daughon.buttons.buttonHandler();
+  //var buttonHandler = new daughon.buttons.buttonHandler();
 
   // Has to be in an array to be parsed
-  var definedActions = [new daughon.buttons.button(0,1,-1), 
-                        new daughon.buttons.button(1,1,-1),
-                        new daughon.buttons.button(2,1,-1),
-                        new daughon.buttons.button(0,1,0),
-                        new daughon.buttons.button(1,1,0),
-                        new daughon.buttons.button(5,1,-1)];
+  var buttons = [new daughon.buttons.Button(0,1,-1), 
+                        new daughon.buttons.Button(1,1,-1),
+                        new daughon.buttons.Button(2,1,-1),
+                        new daughon.buttons.Button(0,1,0),
+                        new daughon.buttons.Button(1,1,0),
+                        new daughon.buttons.Button(5,1,-1)];
 
    // Update is the information from the panel, you can parse it to get things like color and other data
-  definedActions[0].toggle = function(update){ // Bedroom Fan
+  buttons[0].toggle = function(update){ // Bedroom Fan
     if(items.getItem('BedroomFan_Power').state.toString() == "ON"){
       items.getItem('BedroomFan_Power').sendCommand("OFF");
       } else {
@@ -63,7 +59,7 @@ function wrapper(){
      }   
   }
   
-  definedActions[1].toggle = function(update){ // Closet Light
+  buttons[1].toggle = function(update){ // Closet Light
     if(items.getItem('BedroomClosetLight_Power').state.toString() == "ON"){
       items.getItem('BedroomClosetLight_Power').sendCommand("OFF");
       } else {
@@ -71,7 +67,7 @@ function wrapper(){
      }   
   }
   
-  definedActions[2].toggle = function(update){ // RGBW Lights
+  buttons[2].toggle = function(update){ // RGBW Lights
     if(items.getItem('BedroomLightOne_Power').state.toString() == "ON"){
         items.getItem('BedroomLightOne_Power').sendCommand("OFF");
         items.getItem('BedroomLightThree_Power').sendCommand("OFF");
@@ -83,7 +79,7 @@ function wrapper(){
        }   
   }
   
-  definedActions[2].color = function(update){
+  buttons[2].color = function(update){
     var HSBType = Java.type('org.openhab.core.library.types.HSBType');
     var r,g,b;
     r = Math.round(parseInt(update["data"]["r"]));
@@ -100,7 +96,7 @@ function wrapper(){
     items.getItem('BedroomLightTwo_Power').sendCommand("ON");
   }
   
-  definedActions[2].white = function(update){
+  buttons[2].white = function(update){
     items.getItem('BedroomLightOne_White').sendCommand(update["data"]["w"]);
     items.getItem('BedroomLightThree_White').sendCommand(update["data"]["w"]);
     items.getItem('BedroomLightTwo_White').sendCommand(update["data"]["w"]);
@@ -109,14 +105,14 @@ function wrapper(){
     items.getItem('BedroomLightTwo_Power').sendCommand("ON");
   }
 
-  definedActions[3].run = function(update){ // Static Blue
+  buttons[3].run = function(update){ // Static Blue
     items.getItem('BedroomLightOne_Color').sendCommand("250,100,100");
     items.getItem('BedroomLightTwo_Color').sendCommand("250,100,100");
     items.getItem('BedroomLightThree_Color').sendCommand("250,100,100");
     items.getItem('BedroomDeskStrip_Color').sendCommand("250,100,100");
   }
 
-  definedActions[4].run = function(update){ // Static All Off
+  buttons[4].run = function(update){ // Static All Off
     items.getItem('BedroomLightOne_Power').sendCommand("OFF");
     items.getItem('BedroomLightThree_Power').sendCommand("OFF");
     items.getItem('BedroomLightTwo_Power').sendCommand("OFF");
@@ -124,9 +120,10 @@ function wrapper(){
     items.getItem('BedroomFan_Power').sendCommand("OFF");
     items.getItem('BedroomDeskStrip_Power').sendCommand("OFF");
   }
+  // mqttBroker,commandTopic,submenuItem,depthItem,pageItem,submenuArrayItem,telegram,submenus,pages,people,buttons
+  var Panel = new daughon.Panel("mqtt:broker:x","cmnd/tasmota_bedroom_nspanel/nextion","BedroomNSPanel_Submenu","BedroomNSPanel_Depth","BedroomNSPanel_Page","BedroomNSPanel_SubmenuList","telegram:telegramBot:x",submenus,pages,people,buttons)
+  Panel.runPanel(update);
 
 
-  // update,messageHandler,people,pageHandler,pages,indicatorHandler,buttonHandler,definedActions
-  daughon.main.runPanel(jsonObject,messageHandler,people,pageHandler,pages,indicatorHandler,buttonHandler,definedActions,submenuHandler,submenus);
   
 }
